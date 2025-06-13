159db8ab6398fce84c3580d92af80cf608fe06ab
muge <100340171+hromuge@users.noreply.github.com>
2025-06-13 23:45:43 +0500

    small changes to design

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import {Skillcard} from './skillcard/skillcard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Skillcard],
+  imports: [Skillcard],
   templateUrl: './app.html',
   standalone: true,
   styleUrl: './app.css'
 })
-export class App {
-  bgColor1 = '#232147';
-  bgColor2 = '#23213B';
-}
+export class App {}
