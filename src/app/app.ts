2943b07d2250e01000b26f37f4de919ae877f2b7
muge <muge@hroesports.ch>
2026-03-06 16:10:46 +0100

    refactor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import {Skillcard} from './skillcard/skillcard';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [Skillcard],
+  imports: [Skillcard, MatIcon],
   templateUrl: './app.html',
   standalone: true,
   styleUrl: './app.css'
